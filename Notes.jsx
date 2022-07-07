98e00d910fbef7b12007e033ba3c3b7f9e72007b
Kannanravi <108415415+Kannxnravi@users.noreply.github.com>
2022-07-07 16:26:39 +0530

    Windows Explorer

---
+++ Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+;
+
+function Notes() {
+  return  <div className="note">
+    <h1>This is the React Bootcamp</h1>
+    <p>
+      Growth of the organization as well as to enhance my knowledge about new and emerging
+      trends in the IT sector.
+    </p>
+  </div>
+}
+
+export default Notes;
